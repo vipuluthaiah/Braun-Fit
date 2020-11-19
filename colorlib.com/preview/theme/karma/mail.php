33123156a90c7221224be0ac397f42db2ea9069a
vipuluthaiah <kechamadavipul@gmail.com>
2020-11-19 23:26:58 +0530

    mail tesat2

--- colorlib.com/preview/theme/karma/mail.php
+++ colorlib.com/preview/theme/karma/mail.php
@@ -4,13 +4,13 @@
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'kechamadavipul@gmail.com';
+    $email_from = 'braunfitblr@gmail.com';
     $email_subject = "New Form Submission";
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email .\n".
                         "User Message: $message.\n";
     
-    $to = "kechamadavipul@gmail.com";
+    $to = "braunfitblr@gmail.com";
     $header = "From: $email_from \r\n";
     mail($to, $email_subject, $email_body, $header);
     header("Location: contact.html");
